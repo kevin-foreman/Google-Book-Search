94e2b8bc5c50322f945eb31e3189c18cdebac256
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-07-03 15:28:50 -0500

    Updated file structure

---
+++ client/build/precache-manifest.28b4c7cd4ea0f5aa80bb9c41dd4fa7bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f73a7dca0959e4deb458616bd5affb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6773d2ca075856fb64f9",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/js/2.27b3ac98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27b3ac98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6773d2ca075856fb64f9",
+    "url": "/static/js/main.256c1367.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
