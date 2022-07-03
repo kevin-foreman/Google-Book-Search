3e3b3f6b8946faaef6cf0af273d272603ac9dcdc
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-07-03 16:07:23 -0500

    Added schemas, typeDefs, and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+    }
+
+    type Book {
+        authors: String
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+`;
+
+module.exports = typeDefs;
