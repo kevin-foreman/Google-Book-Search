aa172450d3d21fde2a2b81441c04b723db60966b
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-07-04 10:26:53 -0500

    Added some types to typeDefs, figuring out last one

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,6 +16,25 @@ const typeDefs = gql`
         link: String
         title: String!
     }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        userById(_id: ID!): User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput!): User
+        removeBook(bookId: String!): User
+    }
 `;
 
 module.exports = typeDefs;
