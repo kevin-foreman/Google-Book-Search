d3c11e9f81c3b3a0ccd9c794c892b9563f170d37
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-07-06 14:13:14 -0500

    Found errors in app.js with the routes

--- client/src/App.js
+++ client/src/App.js
@@ -50,15 +50,14 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={SearchBooks} />
-          <Route exact path='/saved' element={SavedBooks} />
-          <Route component={SearchBooks} />
+          <Route exact path='/' element={ <SearchBooks /> } />
+          <Route exact path='/saved' element={ <SavedBooks /> } />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Routes>
       </>
     </Router>
     </ApolloProvider>
   );
-};
+}
 
 export default App;
